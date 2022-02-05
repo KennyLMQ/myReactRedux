7bdefd24b4b57686a1eff0fb94969787cc78053a
Kenny <22737987+KennyLMQ@users.noreply.github.com>
2022-02-05 23:31:30 +0800

    build: add mui packages

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import { Provider } from 'react-redux';
 import App from './App';
 import { store } from './app/store';
 import * as serviceWorker from './serviceWorker';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.render(
   <React.StrictMode>
